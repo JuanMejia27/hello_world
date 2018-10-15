b8b48fcea7ad44cd5b1b56b9c18492a1a8394079
vagrant <vagrant@vagrant.vm>
2018-10-15 21:43:51 +0000

    fix: correccion de la ruta2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root :to => 'welcome#index'
-match '/saludo', :to => 'home#saludo', :via => [:get]
+  match '/saludo', :to => 'welcome#saludo', :via => [:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
