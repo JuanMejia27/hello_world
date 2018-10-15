697e535b6b7221d1ac72fdc12176a1fa6b7bd7a7
vagrant <vagrant@vagrant.vm>
2018-10-15 21:40:22 +0000

    fix: correccion de la ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/saludo'
-  root :to => 'welcome#saludo'
+  get 'welcome/index'
+  root :to => 'welcome#index'
 match '/saludo', :to => 'home#saludo', :via => [:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
